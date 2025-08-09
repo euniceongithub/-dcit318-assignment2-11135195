4b70202951da5f935c5189b27979976a07180762
euniceakligo <eyakligo@st.ug.edu.gh>
2025-08-09 11:32:40 +0000

    Inheritance

---
+++ Inheritance.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace InheritanceExample
+{
+    // Base class
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    // Derived class Dog
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    // Derived class Cat
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal genericAnimal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            genericAnimal.MakeSound(); // Output: Some generic sound
+            dog.MakeSound();           // Output: Bark
+            cat.MakeSound();           // Output: Meow
+
+            Console.ReadLine();
+        }
+    }
+}
